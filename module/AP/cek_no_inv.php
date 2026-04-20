8d16da55a67ad2ec54a8d3f7f45b45480386a323
Druhiyat24 <dederuhiyat24@gmail.com>
2026-04-20 09:21:49 +0700

    fix insert IR

--- module/AP/cek_no_inv.php
+++ module/AP/cek_no_inv.php
@@ -7,9 +7,9 @@ $no_inv = $_POST['no_inv']; // array
 $list = "'" . implode("','", $no_inv) . "'";
 
 $q = mysqli_query($conn2,"
-    SELECT no_invoice 
-    FROM ir_invoice_supp
-    WHERE no_invoice IN ($list) and status = 'Y' and no_invoice != '-'
+    SELECT a.no_invoice 
+    FROM ir_invoice_supp a inner join ir_invoice_supp_h b on b.doc_number = a.doc_number
+    WHERE a.no_invoice IN ($list) and a.status = 'Y' and b.status != 'cancel' and a.no_invoice != '-'
 ");
 
 $data = [];
