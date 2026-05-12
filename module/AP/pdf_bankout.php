652daf3901d80073a14f487cfaa3b3d11fd0db9d
Druhiyat24 <dederuhiyat24@gmail.com>
2026-05-12 09:12:30 +0700

    adjust

--- module/AP/pdf_bankout.php
+++ module/AP/pdf_bankout.php
@@ -15,7 +15,11 @@ if ($no_bankout == 'BK/BCA1979/NAG/0525/00339') {
     select no_coa,nama_coa,nama_costcenter,b.profit_center,reff_doc,reff_date,curr, debit, credit, keterangan from tbl_list_journal a left join b_master_cc b on b.no_cc = a.no_costcenter where no_journal = '$no_bankout' AND ( debit > 0 OR credit > 0) and no_coa != '1.10.01' and a.status != 'Updated'";
 }else{
 
-    $sqlys = "select no_coa,nama_coa,nama_costcenter,c.nama_pc profit_center,reff_doc,reff_date,curr,debit,credit, a.keterangan from tbl_list_journal a left join b_master_cc b on b.no_cc = a.no_costcenter left join master_pc c on c.kode_pc = a.profit_center where a.status != 'Updated' AND (debit > 0 and no_journal = '$no_bankout' || credit > 0 and no_journal = '$no_bankout' and a.status != 'Updated')";
+    if ($rs['reff_doc'] == 'Payment') {
+        $sqlys = "select no_coa,nama_coa,nama_costcenter,c.nama_pc profit_center,reff_doc,reff_date,curr,debit,credit, a.keterangan from tbl_list_journal a left join b_master_cc b on b.no_cc = a.no_costcenter left join master_pc c on c.kode_pc = a.profit_center where a.status != 'Updated' AND (debit > 0 and no_journal = '$no_bankout' || credit > 0 and no_journal = '$no_bankout' and a.status != 'Updated') and a.nama_coa not like '%SELISIH KURS%'";
+    }else{
+        $sqlys = "select no_coa,nama_coa,nama_costcenter,c.nama_pc profit_center,reff_doc,reff_date,curr,debit,credit, a.keterangan from tbl_list_journal a left join b_master_cc b on b.no_cc = a.no_costcenter left join master_pc c on c.kode_pc = a.profit_center where a.status != 'Updated' AND (debit > 0 and no_journal = '$no_bankout' || credit > 0 and no_journal = '$no_bankout' and a.status != 'Updated')";
+    }
 }
 
 ob_start();
