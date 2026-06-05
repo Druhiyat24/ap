71bf047b929f455990d8a129bad2892575c10306
Dede Ruhiyat <95616970+Druhiyat24@users.noreply.github.com>
2026-06-05 16:27:00 +0700

    negatif value rekonsiliasi RO/OUT

--- module/AP/ekspor_rekonsiliasi_jurnal_bpb.php
+++ module/AP/ekspor_rekonsiliasi_jurnal_bpb.php
@@ -78,7 +78,7 @@ bpb_sb as (select bpbno_int, bpbdate, supplier, ph.pono, phd.tipe_com, IF(bpb.co
 
 bpb_knitting as (select no_bpb, tgl_bpb, supplier, no_po, tipe, IF(confirm = 'Y', 'APPROVED', 'DRAFT') status, a.curr, SUM(total) total, SUM(dpp) dpp, SUM(ppn) ppn from bpb_knitting a INNER JOIN (select bpbno_int, confirm from bpb where bpbdate BETWEEN '$start_date' and '$end_date' and cancel != 'Y' and (bpbno_int like 'BPB/NAK%' OR bpbno_int like 'GB/IN%' OR bpbno_int like 'SGG/IN%'  OR bpbno_int like 'GG/IN%'  OR bpbno_int like 'GM/IN%') GROUP BY bpbno_int) b on b.bpbno_int = a.no_bpb GROUP BY no_bpb),
 
-bppb as (select bppbno_int, bppbdate, supplier, b.pono, tipe_com tipe, IF(confirm = 'Y', 'APPROVED', 'DRAFT') status, curr, (dpp + (dpp * (coalesce(tax,0)/100))) total,dpp,(dpp * (coalesce(tax,0)/100)) ppn from (select bppbno, bppbno_int, bppb.bppbdate, bppb.id_supplier, supplier, mattype, n_code_category, if(matclass like '%ACCESORIES%','ACCESORIES',mi.matclass) matclass, bppb.curr,bppb.username, bppb.dateinput, bppb.confirm, SUM(((qty) * price)) as dpp,bpbno_ro from bppb inner join masteritem mi on bppb.id_item = mi.id_item inner join mastersupplier ms on bppb.id_supplier = ms.id_supplier where bppbdate BETWEEN '$start_date' and '$end_date' and cancel != 'Y' and bppbno_int not like 'OFC/%' group by bppbno_int) a left join (select bpbno,pono from bpb GROUP BY bpbno) b on b.bpbno = a.bpbno_ro left JOIN (select ph.pono,ph.tax, tipe_com from po_header ph left join po_header_draft phd on phd.id = ph.id_draft GROUP BY pono) c on c.pono = b.pono),
+bppb as (select bppbno_int, bppbdate, supplier, b.pono, tipe_com tipe, IF(confirm = 'Y', 'APPROVED', 'DRAFT') status, curr, (dpp + (dpp * (coalesce(tax,0)/100))) total,dpp,(dpp * (coalesce(tax,0)/100)) ppn from (select bppbno, bppbno_int, bppb.bppbdate, bppb.id_supplier, supplier, mattype, n_code_category, if(matclass like '%ACCESORIES%','ACCESORIES',mi.matclass) matclass, bppb.curr,bppb.username, bppb.dateinput, bppb.confirm, - SUM(((qty) * price)) as dpp,bpbno_ro from bppb inner join masteritem mi on bppb.id_item = mi.id_item inner join mastersupplier ms on bppb.id_supplier = ms.id_supplier where bppbdate BETWEEN '$start_date' and '$end_date' and cancel != 'Y' and bppbno_int not like 'OFC/%' group by bppbno_int) a left join (select bpbno,pono from bpb GROUP BY bpbno) b on b.bpbno = a.bpbno_ro left JOIN (select ph.pono,ph.tax, tipe_com from po_header ph left join po_header_draft phd on phd.id = ph.id_draft GROUP BY pono) c on c.pono = b.pono),
 
 data_bpb as (select bpbno_int, bpbdate, supplier, COALESCE(pono,'-') pono, tipe_com, status, curr, round(total,4) total, round(dpp,4) dpp, round(ppn ,4) ppn from (select * from bpb_sb where total != 0
 UNION ALL
@@ -86,29 +86,29 @@ select * from bpb_knitting where total != 0
 UNION ALL
 select * from bppb where total != 0) a),
 
-data_jurnal as (SELECT no_journal, ABS( SUM(
+data_jurnal as (SELECT no_journal, SUM(
             CASE 
                 WHEN nama_coa LIKE 'GR/IR%' 
                      OR nama_coa LIKE 'PIUTANG LAIN-LAIN%'
-                THEN (debit - credit)
+                THEN (credit - debit)
                 ELSE 0 
-            END )) AS total, 
-                        ABS( SUM(
+            END ) AS total, 
+                        SUM(
             CASE 
                 WHEN nama_coa LIKE 'PERSEDIAAN%' 
                      OR nama_coa LIKE 'BEBAN%'
                                          OR nama_coa LIKE 'MESIN%'
                 THEN (debit - credit)
                 ELSE 0 
-            END ) ) AS dpp,
-                        ABS( SUM(
+            END ) AS dpp,
+                        SUM(
             CASE 
                         
                 WHEN nama_coa LIKE 'PAJAK%' 
                      OR nama_coa LIKE 'PENDAPATAN%'
                 THEN (debit - credit)
                 ELSE 0 
-            END ) ) AS ppn FROM tbl_list_journal WHERE tgl_journal BETWEEN '$start_date' and '$end_date' AND type_journal LIKE '%AP - BPB%' GROUP BY no_journal)
+            END ) AS ppn FROM tbl_list_journal WHERE tgl_journal BETWEEN '$start_date' and '$end_date' AND type_journal LIKE '%AP - BPB%' GROUP BY no_journal )
                         
 select a.*, ROUND(COALESCE(b.total,0),4) total_jurnal, ROUND(COALESCE(b.dpp,0),4) dpp_jurnal, ROUND(COALESCE(b.ppn,0),4) ppn_jurnal, ROUND(a.total - ROUND(COALESCE(b.total,0),4),4) diff_total, ROUND(a.dpp - ROUND(COALESCE(b.dpp,0),4),4) diff_dpp, ROUND(a.ppn - ROUND(COALESCE(b.ppn,0),4),4) diff_ppn from data_bpb a LEFT JOIN data_jurnal b on b.no_journal = a.bpbno_int $status
 ");
